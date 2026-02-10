a4bb9577207fce50745b7d39c5ccecbf89ef5f55
avalenca7474 <adrianofacul.unicap74@gmail.com>
2026-02-10 00:09:55 +0000

    alteração

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>Meu Site</h1>
         {children}
       </body>
     </html>
